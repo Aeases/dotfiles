59a3596e7e96ea038814f1e8f270d2ffd935d1c2
Aeases <96164686+Aeases@users.noreply.github.com>
2024-12-28 18:43:48 +0800

    ner

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -9,7 +9,45 @@ return {
         theme = 'auto',
         component_separators = '',
         section_separators = { left = '', right = '' },
-      },
-    },
+        disabled_filetypes = {
+              statusline = {},
+              winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            always_show_tabline = false,
+            globalstatus = false,
+            refresh = {
+              statusline = 100,
+              tabline = 100,
+              winbar = 100,
+            }
+          },
+          sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = { {'filename', file_status = true, newfile_status = true, symbols = {
+              modified = '●',
+              readonly = '🔒',
+              unnamed = '名無し',
+              newfile = '新',
+        } } },
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {{'datetime', style="%H時%M分"}},
+            lualine_z = {'location'},
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+          },
+          tabline = {},
+          winbar = {},
+          inactive_winbar = {},
+          extensions = {}
+        },
   },
 }
