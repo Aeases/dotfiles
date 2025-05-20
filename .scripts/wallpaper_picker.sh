7f435ff68a06cb062ee3f734e5504f8404e5f9a8
Aeases <96164686+Aeases@users.noreply.github.com>
2025-05-20 23:53:06 +0800

    upped quality of pixel art but goodly & added new

--- .scripts/wallpaper_picker.sh
+++ .scripts/wallpaper_picker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swww img "$(ls ${HOME}/.config/hypr/wallpapers/ | tofi | xargs -I {} echo "${HOME}/.config/hypr/wallpapers/{}")"
+swww img --transition-type none "$(ls ${HOME}/.config/hypr/wallpapers/ | tofi | xargs -I {} echo "${HOME}/.config/hypr/wallpapers/{}")"
