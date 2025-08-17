031a310a5cf3d8019f51650debb2beffae94f184
Aeases <96164686+Aeases@users.noreply.github.com>
2025-08-17 18:07:37 +0800

    some updates

--- .scripts/wallpaper_picker.sh
+++ .scripts/wallpaper_picker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swww img --transition-type none "$(ls ${HOME}/.config/hypr/wallpapers/ | tofi | xargs -I {} echo "${HOME}/.config/hypr/wallpapers/{}")"
+swww img --transition-type wave --transition-fps 60 "$(ls ${HOME}/.config/hypr/wallpapers/ | tofi | xargs -I {} echo "${HOME}/.config/hypr/wallpapers/{}")"
