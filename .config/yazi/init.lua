9cacd6cbc0779923e1bdd8d4ac9ff634f804d48c
Aeases <96164686+Aeases@users.noreply.github.com>
2026-01-26 22:16:31 +0800

    bruh

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -2,4 +2,11 @@ require("no-status"):setup()
 require("smart-enter"):setup({
 	open_multi = true,
 })
-require("relative-motions"):setup({ show_numbers = "relative_absolute" })
+
+require("relative-motions"):setup({
+	show_numbers = "relative_absolute",
+	line_numbers_styles = {
+		hovered = ui.Style():bold():reverse(true),
+		normal = ui.Style(),
+	},
+})
